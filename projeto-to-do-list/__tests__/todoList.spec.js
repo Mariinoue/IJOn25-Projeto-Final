a291cd4ff642789cb0a8f33f24a19175c07ac017
Mari <marinoue@gmail.com>
2023-09-14 00:42:43 -0300

    chore: change-directory-project

---
+++ projeto-to-do-list/__tests__/todoList.spec.js
@@ -0,0 +1,30 @@
+import  List  from "../src/todoList.js";
+
+describe('List', () => {
+  let todoList;
+
+  beforeEach(() => {
+    todoList = new List();
+  });
+
+  it('should add an item to the list', () => {
+    todoList.addTodoItem('Fazer o Projeto final');
+    expect(todoList.getTodoItems()).toEqual(['Fazer o Projeto final']);
+  });
+
+  it('should remove an item from the list', () => {
+    todoList.addTodoItem('Fazer o Projeto final');
+    todoList.removeTodoItem('Fazer o Projeto final');
+    expect(todoList.getTodoItems()).toEqual([]);
+  });
+
+  it('should return the list of items', () => {
+    todoList.addTodoItem('Fazer o Projeto final');
+    todoList.addTodoItem('Pagar conta');
+    expect(todoList.getTodoItems()).toEqual(['Fazer o Projeto final', 'Pagar conta']);
+  });
+});
+
+
+
+
